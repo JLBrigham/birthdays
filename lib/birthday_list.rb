cb8811c5edab88467bd7c000b9a2ddee8b1d28a6
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-11 18:15:59 +0100

    can list friends bdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,19 @@
 
+class BirthdayList
+attr_reader :list
+
+def initialize
+  @list = []
+end
+
+def add(name, birthday)
+@list << {name: name, birthday: birthday}
+end
+
+def print
+  @list.each do |bday|
+    puts "#{bday[:name]}: #{bday[:birthday]}"
+  end
+end
+
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,17 @@
+require'birthday_list'
 
+describe BirthdayList do
+  let (:subject) {subject = BirthdayList.new}
+
+
+it 'should add a friends name and birthday' do
+expect(subject.add("joanna", "21/09/90")).to eq [{name: "joanna", birthday: "21/09/90"}]
+end
+
+it 'should print a list of all names and birthdays' do
+subject.add("joanna", "21/09/90")
+subject.add("andrew", "30/10/93")
+expect{subject.print}.to output("joanna: 21/09/90\nandrew: 30/10/93\n").to_stdout
+end
+
+end
